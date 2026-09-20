de3952cadc2f2f812714501c676090cfd0ab5459
Sanat Konda <138330537+sskonda@users.noreply.github.com>
2026-09-20 16:16:30 -0400

    created rtl files

---
+++ FPGA/rtl/axis_passthrough_wrapper.v
@@ -0,0 +1,56 @@
+module axis_passthrough_wrapper #(
+    parameter DATA_WIDTH = 32
+) (
+    (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 ACLK CLK" *)
+    (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME ACLK, ASSOCIATED_BUSIF S_AXIS:M_AXIS, ASSOCIATED_RESET ARESETN" *)
+    input wire aclk,
+
+    (* X_INTERFACE_INFO = "xilinx.com:signal:reset:1.0 ARESETN RST" *)
+    (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME ARESETN, POLARITY ACTIVE_LOW" *)
+    input wire aresetn,
+
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 S_AXIS TDATA" *)
+    (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME S_AXIS, TDATA_NUM_BYTES 4, HAS_TLAST 1, HAS_TKEEP 0, HAS_TSTRB 0" *)
+    input wire [DATA_WIDTH-1:0] s_axis_tdata,
+
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 S_AXIS TVALID" *)
+    input wire s_axis_tvalid,
+
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 S_AXIS TREADY" *)
+    output wire s_axis_tready,
+
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 S_AXIS TLAST" *)
+    input wire s_axis_tlast,
+
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 M_AXIS TDATA" *)
+    (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME M_AXIS, TDATA_NUM_BYTES 4, HAS_TLAST 1, HAS_TKEEP 0, HAS_TSTRB 0" *)
+    output wire [DATA_WIDTH-1:0] m_axis_tdata,
+
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 M_AXIS TVALID" *)
+    output wire m_axis_tvalid,
+
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 M_AXIS TREADY" *)
+    input wire m_axis_tready,
+
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 M_AXIS TLAST" *)
+    output wire m_axis_tlast
+);
+
+    axis_passthrough #(
+        .DATA_WIDTH(DATA_WIDTH)
+    ) passthrough_inst (
+        .aclk          (aclk),
+        .aresetn       (aresetn),
+
+        .s_axis_tdata  (s_axis_tdata),
+        .s_axis_tvalid (s_axis_tvalid),
+        .s_axis_tready (s_axis_tready),
+        .s_axis_tlast  (s_axis_tlast),
+
+        .m_axis_tdata  (m_axis_tdata),
+        .m_axis_tvalid (m_axis_tvalid),
+        .m_axis_tready (m_axis_tready),
+        .m_axis_tlast  (m_axis_tlast)
+    );
+
+endmodule
